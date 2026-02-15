c75a83859fdd6d0352945cdd10b69b3c43615dee
Alman <215115964+Alman8904@users.noreply.github.com>
2026-02-15 17:09:41 +0530

    add UpdateUserSkill component

--- src/Components/UserSkills/MySkills.jsx
+++ src/Components/UserSkills/MySkills.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { authFetch } from "../jwt-storage/authFetch";
+import UpdateUserSkill from "./UpdateUserSkill";
 
 export default function MySkills() {
 
